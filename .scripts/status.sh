6ef7381ac6143ac4f1ba47d5f2e1faed848b14f7
Edoardo Annunziata <annunziata.edoardo@outlook.com>
2026-05-26 16:29:52 +0200

    update status script

--- .scripts/status.sh
+++ .scripts/status.sh
@@ -11,7 +11,18 @@ conn() {
 conn
 
 bat() {
-    rbat=$(upower -b | grep percentage | awk '{print $2}')
+    local bat=$(
+        upower -b \
+        | grep percentage \
+        | awk '{print $2}' \
+        | sed 's/%//g'
+    )
+    local status=$(
+        upower -b \
+        | grep state \
+        | awk '{if (($2=="charging") || ($2=="fully-charged")) print "+"; else print "%"}'
+    )
+    rbat=$(printf '%s%s' $bat $status)
 }
 bat
 
